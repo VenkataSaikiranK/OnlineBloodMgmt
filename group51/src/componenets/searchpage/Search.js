f36fc283d00ae2d643e7a65599e81ce87f74a339
Vaishnavi Jupudi <vaishnavijupudi@Vaishnavis-MacBook-Air.local>
2022-04-06 00:59:31 +0530

    Search page

--- group51/src/componenets/searchpage/Search.js
+++ group51/src/componenets/searchpage/Search.js
@@ -6,6 +6,13 @@ import axios from "axios";
 export default function Search() {
     const [city, setCity] = useState("Halifax");
     const [bloodGroup, setBloodGroup] = useState("");
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [number, setNumber] = useState("");
+    const [error, setError] = useState("");
+    const [user, setUser] = useState("");
+
+
     const validateData = () => {
         console.log("in validate");
         var data = {
@@ -13,11 +20,43 @@ export default function Search() {
             bloodGroup: bloodGroup,
         };
         axios.post("http://localhost:9000/searchUsers", data).then((response) => {
-           console.log("in axios post");
-           console.log("data is" + data);
-            console.log(response);
+            console.log(response.data);
+            if(response.data){
+                setUser(response.data);
+                displayUsers()
+
+            }else{
+                return(
+                    <p> There are no users available</p>
+                )
+            }
+
         });
     }
+    function users(){
+        return user;
+    }
+    function displayUsers(){
+        if(user){
+            return(
+                <>
+
+                    <div className="card" style={{ width: "18rem" }}>
+                        {users().map((data, id) => (
+                            <div className="card-body">
+                                <h5 className="card-title"> {data.firstname} {data.lastname}</h5>
+                                <p className="card-text">{data.email}</p>
+                                <p className="card-text">{data.phone}</p>
+                            </div>
+                        ))};
+                    </div>
+
+                </>
+            )
+        }
+
+    }
+
 
     return(
         <div>
@@ -56,6 +95,14 @@ export default function Search() {
     <div>
         <button className="btn btn-primary"  type="button" onClick={validateData}>Search</button>
     </div>
+            {error ? (
+                <div style={{ marginLeft: "5px" }}>
+                   {error}
+                </div>
+            ) : (
+                displayUsers()
+            )}
+
         </div>
     )
 }
